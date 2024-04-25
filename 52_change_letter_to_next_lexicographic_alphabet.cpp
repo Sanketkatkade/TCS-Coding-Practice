552f0b56b733363f4ceb28f92d80dcf988b3bf0d
Sanket <sanketkatkade782@gmail.com>
2024-04-25 13:08:25 +0530

    update the file

--- 52_change_letter_to_next_lexicographic_alphabet.cpp
+++ 52_change_letter_to_next_lexicographic_alphabet.cpp
@@ -1,28 +1,31 @@
 // https://takeuforward.org/data-structure/change-every-letter-with-next-lexicographic-alphabet/
 
-
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
+int main()
+{
     string str;
     cin >> str;
 
-    for(int i = 0; i < str.size(); i++){
+    for (int i = 0; i < str.size(); i++)
+    {
         int ascii = (int)(str[i]);
-        if(ascii == 90){
+        if (ascii == 90)
+        {
             str[i] = char(65);
         }
-        else if(ascii == 122){
+        else if (ascii == 122)
+        {
             str[i] = char(97);
         }
-        else if((ascii >= 65 && ascii < 90) || (ascii >= 97 && ascii < 122)){
+        else if ((ascii >= 65 && ascii < 90) || (ascii >= 97 && ascii < 122))
+        {
             str[i] = char(ascii + 1);
         }
     }
+    cout << str;
 
-    cout<<str;
-    
     return 0;
 }
